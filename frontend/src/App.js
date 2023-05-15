b20807991a76cf02f0a998051075635cb5e559a2
Eduard <505281@student.fontys.nl>
2023-05-15 20:00:42 +0200

    Import header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
+      <Header />
       <main>
         <Container fluid>
           <Routes>
