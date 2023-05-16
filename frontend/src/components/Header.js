bc7c2c3d6a984164b13f82b666c15feeecac29bd
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-16 18:31:21 +0200

    Create initial design of the header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <nav className='flex nav-flex'>
+      <div className='logo'>
+        <img src='logo.svg' alt='logo' />
+      </div>
+
+      <ul className='flex nav-list'>
+        <li>
+          <a href='#'>Explore</a>
+        </li>
+        <li>
+          <a href='#'>Testimonials</a>
+        </li>
+        <li>
+          <a href='#'>Premium</a>
+        </li>
+        <li>
+          <a href='#'>Try It</a>
+        </li>
+        <li>
+          <a href='#'>Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Header;
