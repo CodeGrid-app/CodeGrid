2a1f68b6db59806bb4d30f346b9dac6d537a9a36
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-17 20:22:19 +0200

    Create half of the Login screen.

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import {
+  GoogleLoginButton,
+  GithubLoginButton,
+  FacebookLoginButton,
+} from 'react-social-login-buttons';
+import Form from 'react-bootstrap/Form';
 
 function LoginScreen() {
-  return <div>LoginScreen</div>;
+  return (
+    <div className='login-screen flex'>
+      <div className='login-container flex'>
+        <div className='login-left flex'>
+          <div className='logo'>
+            <img src='/logo.svg' alt='logo' />
+          </div>
+
+          <div className='login-buttons flex'>
+            <GoogleLoginButton
+              className='social-button'
+              size='40px'
+            ></GoogleLoginButton>
+            <GithubLoginButton
+              className='social-button'
+              size='40px'
+            ></GithubLoginButton>
+            <FacebookLoginButton
+              className='social-button'
+              size='40px'
+            ></FacebookLoginButton>
+          </div>
+
+          <p>
+            Do not have an account? <span>Sign Up Now</span>
+          </p>
+        </div>
+
+        {/* <hr /> */}
+        <div className='login-right flex'></div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginScreen;
