941ec6b5ea0ef10322df8ed435334db980f698f0
eduard-cc <eduard.ciociu22@gmail.com>
2023-05-17 18:35:58 +0200

    Change header title

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,15 +4,16 @@ function HomeScreen() {
   return <section className="flex" id="hero-section">
     <div className="hero-section-container">
       <div className="hero-left flex">
-        <h1>Learn <span>Data Structures</span> & <span>Algorithms</span> in a nice fashion</h1>
-        <p>Unlock your coding potential at <span>CodeGrid</span> - Join now!</p>
+        <h1>Learn <span>Data Structures</span> & <span>Algorithms</span> with ease</h1>
+        <p>Solve coding problems and prepare for technical interviews with <span>CodeGrid</span>. Join now!</p>
         <div className="flex">
           <button className="login-button">Log in</button>
           <button className="create-account-button">Create an account</button>
         </div>
       </div>
-      <div>
-        <img src='logo.svg' alt='logo' style={{width:"100%"}}/>
+
+      <div className="hero-right flex">
+        <img src='hero-icon.svg' alt='logo' />
       </div>
     </div>
   </section>
