ff4f233f2900d64a53d9da07dc51e8c5ea14b12e
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-17 20:34:07 +0200

    Add link to the login button to redirect to '/login'

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function HomeScreen() {
-  return <section className="flex" id="hero-section">
-    <div className="hero-section-container">
-      <div className="hero-left flex">
-        <h1>Learn <span>Data Structures</span> & <span>Algorithms</span> with ease</h1>
-        <p>Solve coding problems and prepare for technical interviews with <span>CodeGrid</span>. Join now!</p>
-        <div className="flex">
-          <button className="create-account-button">Create an account</button>
-          <button className="login-button">Log in</button>
+  return (
+    <section className='flex' id='hero-section'>
+      <div className='hero-section-container'>
+        <div className='hero-left flex'>
+          <h1>
+            Learn <span>Data Structures</span> & <span>Algorithms</span> with
+            ease
+          </h1>
+          <p>
+            Solve coding problems and prepare for technical interviews with{' '}
+            <span>CodeGrid</span>. Join now!
+          </p>
+          <div className='flex'>
+            <button className='create-account-button'>Create an account</button>
+            <Link to='/login'>
+              <button className='login-button'>Log in</button>
+            </Link>
+          </div>
+        </div>
+
+        <div className='hero-right flex'>
+          <img src='hero-icon.svg' alt='logo' />
         </div>
       </div>
-
-      <div className="hero-right flex">
-        <img src='hero-icon.svg' alt='logo' />
-      </div>
-    </div>
-  </section>
+    </section>
+  );
 }
 
 export default HomeScreen;
