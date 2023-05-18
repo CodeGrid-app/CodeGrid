b56a9356105b2ea99f92c65003a2c1719207ff53
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-18 20:12:45 +0200

    Complete left side of the login container & add use state for the email and password

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   GoogleLoginButton,
   GithubLoginButton,
   FacebookLoginButton,
 } from 'react-social-login-buttons';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  console.log(password, email);
   return (
     <div className='login-screen flex'>
       <div className='login-container flex'>
@@ -35,7 +40,34 @@ function LoginScreen() {
           </p>
         </div>
 
-        <div className='login-right flex'></div>
+        <div className='login-right flex'>
+          <Form className='flex'>
+            <Form.Group className='mb-3 flex' controlId='email'>
+              <Form.Control
+                type='email'
+                placeholder='Email Address'
+                value={email}
+                size='lg'
+                onChange={e => setEmail(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className='mb-3 flex' controlId='password'>
+              <Form.Control
+                type='password'
+                placeholder='Password'
+                value={password}
+                size='lg'
+                onChange={e => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <div className='login-div flex'>
+              <Button className='mt-2 mb-2' variant='primary' type='submit'>
+                Log In
+              </Button>{' '}
+              <p>Forgot Password</p>
+            </div>
+          </Form>
+        </div>
       </div>
     </div>
   );
