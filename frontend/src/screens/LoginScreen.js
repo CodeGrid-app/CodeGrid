e4be78d70c56bbda5c7e13a4f731e1cca8f7837f
eduard-cc <eduard.ciociu22@gmail.com>
2023-05-23 21:53:52 +0200

    change layout of login screen

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -6,6 +6,8 @@ import {
 } from 'react-social-login-buttons';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import { FormCheck } from 'react-bootstrap';
 
 function LoginScreen() {
   const [email, setEmail] = useState('');
@@ -14,35 +16,11 @@ function LoginScreen() {
   console.log(password, email);
   return (
     <div className='login-screen flex'>
-      <div className='login-container flex'>
-        <div className='login-left flex'>
-          <div className='logo'>
-            <img src='/logo.svg' alt='logo' />
-          </div>
-
-          <div className='login-buttons flex'>
-            <GoogleLoginButton
-              className='social-button'
-              size='40px'
-            ></GoogleLoginButton>
-            <GithubLoginButton
-              className='social-button'
-              size='40px'
-            ></GithubLoginButton>
-            <FacebookLoginButton
-              className='social-button'
-              size='40px'
-            ></FacebookLoginButton>
-          </div>
-
-          <p>
-            Do not have an account? <span>Sign Up Now</span>
-          </p>
-        </div>
-
-        <div className='login-right flex'>
-          <Form className='flex'>
-            <Form.Group className='mb-3 flex' controlId='email'>
+      <div className='login-container flex direction-column'>
+        <div className='login-top'>
+          <Form>
+            <h1>Log in</h1>
+            <Form.Group controlId='email'>
               <Form.Control
                 type='email'
                 placeholder='Email Address'
@@ -51,7 +29,7 @@ function LoginScreen() {
                 onChange={e => setEmail(e.target.value)}
               />
             </Form.Group>
-            <Form.Group className='mb-3 flex' controlId='password'>
+            <Form.Group controlId='password'>
               <Form.Control
                 type='password'
                 placeholder='Password'
@@ -60,13 +38,30 @@ function LoginScreen() {
                 onChange={e => setPassword(e.target.value)}
               />
             </Form.Group>
-            <div className='login-div flex'>
-              <Button className='mt-2 mb-2' variant='primary' type='submit'>
-                Log In
-              </Button>{' '}
-              <p>Forgot Password</p>
+            <div className='login-utilities flex'>
+              <div className='flex items-center'>
+                <input type='checkbox' id='remember me' name='remember me'></input>
+                <label for='remember me'>Remember me</label>
+              </div>
+              <Link>Forgot password?</Link>
             </div>
+            <Button variant='primary' type='submit'>
+              Log In
+            </Button>{' '}
+            <p>Don't have an account? <Link>Sign up</Link></p>
           </Form>
+        </div>
+        <div className='line-break'><span>OR</span></div>
+        <div className='login-buttons'>
+          <GoogleLoginButton className='social-button'>
+            Google
+          </GoogleLoginButton>
+          <GithubLoginButton className='social-button'>
+            GitHub
+          </GithubLoginButton>
+          <FacebookLoginButton className='social-button'>
+            Facebook
+          </FacebookLoginButton>
         </div>
       </div>
     </div>
