0c1aa54b8c2b248b4858f4b3f61f42b6bf7f8c39
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-23 18:27:50 +0200

    Replace the port number with the port stored in .env for security measurements

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 
-const app = express();
+dotenv.config();
+
+// Connect to MongoDB
 connectDB();
 
+// Initialize express
+const app = express();
+
+// Initialize middleware
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('API is running...');
+  res.send(`API is running...`);
 });
 
-app.listen(5000, () =>
-  console.log('Server running on port 5000'.cyan.bold.underline)
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`Server running on port ${process.env.port}`.cyan.bold.underline)
 );
