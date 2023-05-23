baf9365f0937e427d05cf89640149bb4c7601d8f
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-23 18:15:19 +0200

    Implement method to connect to the DataBase

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,17 @@ import mongoose from 'mongoose';
 
 async function connectDB() {
   try {
-    const conn = await mongoose.connect(`ADD URI HERE`);
+    const conn = await mongoose.connect(
+      `mongodb+srv://DennisEduar:Denniscool2001@codegrid.mh2xdmj.mongodb.net/CodeGrid`
+    );
+
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.brightCyan.bold.underline
+    );
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`.brightRed.bold.underline);
+    process.exit(1);
   }
 }
+
+export default connectDB;
