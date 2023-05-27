555760ac71b5ad2521505072fd5e462c5362dd41
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-27 19:32:03 +0200

    Add passport.authenticate middleware to the login which checks the credentials of a user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
-
+import passport from '../config/passportConfig.js';
 const router = express.Router();
+import { authUser } from '../controllers/userControllers.js';
 
-router.route('/login', authUser).post(authUser);
+router.route('/login').post(passport.authenticate('local'), authUser);
+
+export default router;
