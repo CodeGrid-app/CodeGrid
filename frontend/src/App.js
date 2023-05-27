5ef34aa74d50ee694da2dd6eebc65d042fc952cb
eduard-cc <eduard.ciociu22@gmail.com>
2023-05-27 19:01:29 +0200

    Add route for /signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import Header from './components/Header';
 import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path='/' element={<HomeScreen />}></Route>
             <Route path='/login' element={<LoginScreen />}></Route>
+            <Route path='/signup' element={<SignupScreen />}></Route>
           </Routes>
         </Container>
       </main>
