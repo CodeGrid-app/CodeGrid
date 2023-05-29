6e8b3d4b8fa3b58c61bcf268fbc85ea394133219
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-29 18:16:39 +0200

    Implement the store which has just the login reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,1 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
+import Cookies from 'js-cookie';
+import userLoginSlice from './slices/userSlice';
+
+const cookieValue = Cookies.get('userInfo')
+  ? JSON.parse(Cookies.get('userInfo'))
+  : null;
+
+const initialState = {
+  userLogin: {
+    userInfo: cookieValue || null,
+  },
+};
+
+const store = configureStore({
+  reducer: {
+    userLogin: userLoginSlice,
+  },
+
+  preloadedState: initialState,
+});
+
+export default store;
