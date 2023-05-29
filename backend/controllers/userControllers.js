9d22a3977736b61663b0839f0027700247520c03
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-05-29 17:53:35 +0200

    Add id and name to req.session.userInfo object

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -4,6 +4,10 @@ import AsyncHandler from 'express-async-handler';
 // @route          POST /api/users/login
 // @access         Public
 const authUser = AsyncHandler(async (req, res) => {
+  req.session.userInfo = {
+    id: req.user.id,
+    name: req.user.username,
+  };
   res.json(req.user);
 });
 
@@ -11,4 +15,4 @@ const authUser = AsyncHandler(async (req, res) => {
 //   res.json(req.isAuthenticated());
 // });
 
-export { authUser, getUserName };
+export { authUser };
