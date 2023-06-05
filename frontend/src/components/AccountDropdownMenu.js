c0f0a360d79478f57c23f6a84af47643bd2b8f55
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-06-05 10:39:33 +0200

    File autoformated on save

--- frontend/src/components/AccountDropdownMenu.js
+++ frontend/src/components/AccountDropdownMenu.js
@@ -1,68 +1,62 @@
-import { Menu, Transition } from '@headlessui/react'
+import { Menu, Transition } from '@headlessui/react';
 
 function AccountDropdownMenu() {
   return (
     <Menu>
       <Menu.Button>
         <div id='navbar-avatar' className='navbar-avatar'>
-            <div className='avatar-image-wrapper'>
-            <img
-                src='/images/avatar.png'
-                className='avatar-image'
-            ></img>
-            </div>
+          <div className='avatar-image-wrapper'>
+            <img src='/images/avatar.png' className='avatar-image'></img>
+          </div>
         </div>
       </Menu.Button>
       <Transition
-        enter="transition duration-100 ease-out"
-        enterFrom="transform scale-95 opacity-0"
-        enterTo="transform scale-100 opacity-100"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform scale-100 opacity-100"
-        leaveTo="transform scale-95 opacity-0">
-      <Menu.Items className={'flex direction-column logged-dropdown-menu'}>
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              className={`${active && 'bg-blue-500'} flex username-link items-center`}
-              href="/"
-            >
+        enter='transition duration-100 ease-out'
+        enterFrom='transform scale-95 opacity-0'
+        enterTo='transform scale-100 opacity-100'
+        leave='transition duration-75 ease-out'
+        leaveFrom='transform scale-100 opacity-100'
+        leaveTo='transform scale-95 opacity-0'
+      >
+        <Menu.Items className={'flex direction-column logged-dropdown-menu'}>
+          <Menu.Item>
+            {({ active }) => (
+              <a
+                className={`${
+                  active && 'bg-blue-500'
+                } flex username-link items-center`}
+                href='/'
+              >
                 <div id='navbar-avatar' className='navbar-avatar'>
-                    <div className='avatar-image-wrapper'>
+                  <div className='avatar-image-wrapper'>
                     <img
-                        src='/images/avatar.png'
-                        className='avatar-image'
+                      src='/images/avatar.png'
+                      className='avatar-image'
                     ></img>
-                    </div>
+                  </div>
                 </div>
                 Username
-            </a>
-          )}
-        </Menu.Item>
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              className={`${active}`}
-              href="/"
-            >
-              Submissions
-            </a>
-          )}
-        </Menu.Item>
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              className={`${active}`}
-              href="/"
-            >
-              Sign out
-            </a>
-          )}
-        </Menu.Item>
-      </Menu.Items>
+              </a>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <a className={`${active}`} href='/'>
+                Submissions
+              </a>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <a className={`${active}`} href='/'>
+                Sign out
+              </a>
+            )}
+          </Menu.Item>
+        </Menu.Items>
       </Transition>
     </Menu>
-  )
+  );
 }
 
 export default AccountDropdownMenu;
