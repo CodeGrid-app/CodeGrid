e811b1ec60a230ab77075c077df7985cd21742d4
DennisEduard01 <dennis.eduard01@yahoo.com>
2023-06-05 11:20:10 +0200

    Create error handler middleware

---
+++ backend/middleware/errorMidleware.js
@@ -0,0 +1,9 @@
+function errorHandler(err, req, res, next) {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+  });
+}
+
+export { errorHandler };
